faa1a0ecfb8836600dc6b5590410d858918ced3e
Aakash Jamadar <149180139+AakashJamadar@users.noreply.github.com>
2024-09-13 11:34:25 +0530

    Add files via upload

--- functions.c
+++ functions.c
@@ -1,13 +1,34 @@
+// #include <stdio.h>
+
+// void meow(int n);
+
+// int main(void){
+//    meow(6);
+// }
+
+// void meow(int n){
+//     for(int i=0;i<n;i++){
+//        printf("Meow!\n"); 
+//     }  
+// }
+
 #include <stdio.h>
 
-void meow(int n);
-
-int main(void){
-   meow(6);
+void meowNTimes(int n)
+{
+   for(int i=0;i<n;i++)
+   {
+      printf("Meow!\n");
+   }
 }
 
-void meow(int n){
-    for(int i=0;i<n;i++){
-       printf("Meow!\n"); 
-    }  
+int main(void)
+{
+   int n;
+   do
+   {
+      printf("Enter a number: ");
+      scanf("%d", &n);
+   }while(n<1);  
+   meowNTimes(n);
 }
